f220d2d1e82158131b1d7857f6778187fb58af16
Rinigus <rinigus.git@gmail.com>
2021-05-15 18:46:45 +0300

    update publishing script

--- publish.sh
+++ publish.sh
@@ -4,8 +4,40 @@
 
 set -e
 
+# write index header
+echo '<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Sailfish OS Sony Tama</title>
+        <meta name="description" contents="Sailfish OS Sony Tama Packages">
+    </head>
+    <body>
+      <h1>Sailfish OS Sony Tama Packages</h1>
+      <ul>
+' > RPMS/index.html
+
+
 # generate index.html used to show overview page
-(cd RPMS && tree -h -T "Sailfish OS Packages for Sony Tama" --noreport -D -L 2 -F --dirsfirst -P "*.rpm" -H . . > index.html)
+cd RPMS
+for release in `echo SailfishOS-* | sort -V`; do
+    pwd
+    ls
+    echo $release
+    (cd $release && \
+	 tree -h -T "Sailfish OS Packages for Sony Tama: <br>$release" --noreport \
+	      -D -L 1 -F --dirsfirst -P "*.rpm" -H . . > index.html)
+    echo "<li><a href=\"$release/index.html\">$release</a></li>" >> index.html
+done
+cd ..
+
+# finish main index page
+echo '
+      </ul>
+    </body>
+</html>
+' >> RPMS/index.html
+
 # default error page
 echo "Error, file or directory not found or not accessible via this URL" > RPMS/error.txt
 
